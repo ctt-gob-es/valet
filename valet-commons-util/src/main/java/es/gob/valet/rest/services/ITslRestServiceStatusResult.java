b187bd503451655798c4a41211676d177ab57719
jose.antonio.garcia <jose.antonio.garcia@ESSVQ01L6TJGLQ2.ad.eu.rf-group.org>
2019-02-11 14:54:39 +0100

    11/02/2019 14:55: Se modifican los valores de la clase ITslRestServiceStatusResult de Integer a int para que sean usables en un switch.

--- valet-commons-util/src/main/java/es/gob/valet/rest/services/ITslRestServiceStatusResult.java
+++ valet-commons-util/src/main/java/es/gob/valet/rest/services/ITslRestServiceStatusResult.java
@@ -20,7 +20,7 @@
  * <b>Project:</b><p>Platform for detection and validation of certificates recognized in European TSL.</p>
  * <b>Date:</b><p>26/11/2018.</p>
  * @author Gobierno de España.
- * @version 1.0, 26/11/2018.
+ * @version 1.1, 11/02/2019.
  */
 package es.gob.valet.rest.services;
 
@@ -29,7 +29,7 @@ import es.gob.valet.commons.utils.NumberConstants;
 /**
  * <p>Interface that defines the constants for the result status of the services.</p>
  * <b>Project:</b><p>Platform for detection and validation of certificates recognized in European TSL.</p>
- * @version 1.0, 26/11/2018.
+ * @version 1.1, 11/02/2019.
  */
 public interface ITslRestServiceStatusResult {
 
@@ -37,53 +37,53 @@ public interface ITslRestServiceStatusResult {
 	 * Constant attribute that represents the value for the general result status service
 	 * when there is an error checking the input parameters.
 	 */
-	Integer STATUS_ERROR_INPUT_PARAMETERS = Integer.valueOf(0);
+	int STATUS_ERROR_INPUT_PARAMETERS = 0;
 
 	/**
 	 * Constant attribute that represents the value for the general result status service
 	 * when there is an error executing the service.
 	 */
-	Integer STATUS_ERROR_EXECUTING_SERVICE = Integer.valueOf(1);
+	int STATUS_ERROR_EXECUTING_SERVICE = 1;
 
 	/**
 	 * Constant attribute that represents the value for the result status service
 	 * 'getTslInformation' when the TSL information has been finded.
 	 */
-	Integer STATUS_SERVICE_GETTSLINFORMATION_TSL_INFORMATION_FINDED = Integer.valueOf(NumberConstants.NUM10);
+	int STATUS_SERVICE_GETTSLINFORMATION_TSL_INFORMATION_FINDED = NumberConstants.NUM10;
 
 	/**
 	 * Constant attribute that represents the value for the result status service
 	 * 'getTslInformation' when the TSL information has NOT been finded.
 	 */
-	Integer STATUS_SERVICE_GETTSLINFORMATION_TSL_INFORMATION_NOT_FINDED = Integer.valueOf(NumberConstants.NUM11);
+	int STATUS_SERVICE_GETTSLINFORMATION_TSL_INFORMATION_NOT_FINDED = NumberConstants.NUM11;
 
 	/**
 	 * Constant attribute that represents the value for the result status service
 	 * 'detectCertInTslInfoAndValidation' when there is not a TSL to use with
 	 * the input certificate.
 	 */
-	Integer STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_NOT_FINDED = Integer.valueOf(NumberConstants.NUM20);
+	int STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_NOT_FINDED = NumberConstants.NUM20;
 
 	/**
 	 * Constant attribute that represents the value for the result status service
 	 * 'detectCertInTslInfoAndValidation' when there is a TSL to use with
 	 * the input certificate and the service has been executed.
 	 */
-	Integer STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_FINDED = Integer.valueOf(NumberConstants.NUM21);
+	int STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_FINDED = NumberConstants.NUM21;
 
 	/**
 	 * Constant attribute that represents the value for the result status service
 	 * 'detectCertInTslInfoAndValidation' when there is a TSL to use with
 	 * the input certificate but it has not been detected.
 	 */
-	Integer STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_FINDED_CERT_NOT_DETECTED = Integer.valueOf(NumberConstants.NUM22);
+	int STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_FINDED_CERT_NOT_DETECTED = NumberConstants.NUM22;
 
 	/**
 	 * Constant attribute that represents the value for the result status service
 	 * 'detectCertInTslInfoAndValidation' when there is a TSL to use with
 	 * the input certificate and it has been detected.
 	 */
-	Integer STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_FINDED_CERT_DETECTED = Integer.valueOf(NumberConstants.NUM23);
+	int STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_FINDED_CERT_DETECTED = NumberConstants.NUM23;
 
 	/**
 	 * Constant attribute that represents the value for the result status service
@@ -91,7 +91,7 @@ public interface ITslRestServiceStatusResult {
 	 * the input certificate and it has been detected. The certificate mapping
 	 * information has NOT been collected.
 	 */
-	Integer STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_FINDED_CERT_DETECTED_INFO_NOT_COLLECTED = Integer.valueOf(NumberConstants.NUM24);
+	int STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_FINDED_CERT_DETECTED_INFO_NOT_COLLECTED = NumberConstants.NUM24;
 
 	/**
 	 * Constant attribute that represents the value for the result status service
@@ -99,7 +99,7 @@ public interface ITslRestServiceStatusResult {
 	 * the input certificate and it has been detected. The certificate mapping
 	 * information has been collected.
 	 */
-	Integer STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_FINDED_CERT_DETECTED_INFO_COLLECTED = Integer.valueOf(NumberConstants.NUM25);
+	int STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_FINDED_CERT_DETECTED_INFO_COLLECTED = NumberConstants.NUM25;
 
 	/**
 	 * Constant attribute that represents the value for the result status service
@@ -107,7 +107,7 @@ public interface ITslRestServiceStatusResult {
 	 * the input certificate and it has been detected. The revocation status
 	 * information has NOT been collected.
 	 */
-	Integer STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_FINDED_CERT_DETECTED_REVSTATUS_NOT_COLLECTED = Integer.valueOf(NumberConstants.NUM26);
+	int STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_FINDED_CERT_DETECTED_REVSTATUS_NOT_COLLECTED = NumberConstants.NUM26;
 
 	/**
 	 * Constant attribute that represents the value for the result status service
@@ -115,7 +115,7 @@ public interface ITslRestServiceStatusResult {
 	 * the input certificate and it has been detected. The revocation status
 	 * information has been collected.
 	 */
-	Integer STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_FINDED_CERT_DETECTED_REVSTATUS_COLLECTED = Integer.valueOf(NumberConstants.NUM27);
+	int STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_FINDED_CERT_DETECTED_REVSTATUS_COLLECTED = NumberConstants.NUM27;
 
 	/**
 	 * Constant attribute that represents the value for the result status service
@@ -124,7 +124,7 @@ public interface ITslRestServiceStatusResult {
 	 * information has NOT been collected. The revocation status
 	 * information has NOT been collected.
 	 */
-	Integer STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_FINDED_CERT_DETECTED_INFO_NOT_COLLECTED_REVSTATUS_NOT_COLLECTED = Integer.valueOf(NumberConstants.NUM28);
+	int STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_FINDED_CERT_DETECTED_INFO_NOT_COLLECTED_REVSTATUS_NOT_COLLECTED = NumberConstants.NUM28;
 
 	/**
 	 * Constant attribute that represents the value for the result status service
@@ -133,7 +133,7 @@ public interface ITslRestServiceStatusResult {
 	 * information has NOT been collected. The revocation status
 	 * information has been collected.
 	 */
-	Integer STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_FINDED_CERT_DETECTED_INFO_NOT_COLLECTED_REVSTATUS_COLLECTED = Integer.valueOf(NumberConstants.NUM29);
+	int STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_FINDED_CERT_DETECTED_INFO_NOT_COLLECTED_REVSTATUS_COLLECTED = NumberConstants.NUM29;
 
 	/**
 	 * Constant attribute that represents the value for the result status service
@@ -142,7 +142,7 @@ public interface ITslRestServiceStatusResult {
 	 * information has been collected. The revocation status
 	 * information has NOT been collected.
 	 */
-	Integer STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_FINDED_CERT_DETECTED_INFO_COLLECTED_REVSTATUS_NOT_COLLECTED = Integer.valueOf(NumberConstants.NUM30);
+	int STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_FINDED_CERT_DETECTED_INFO_COLLECTED_REVSTATUS_NOT_COLLECTED = NumberConstants.NUM30;
 
 	/**
 	 * Constant attribute that represents the value for the result status service
@@ -151,6 +151,6 @@ public interface ITslRestServiceStatusResult {
 	 * information has been collected. The revocation status
 	 * information has been collected.
 	 */
-	Integer STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_FINDED_CERT_DETECTED_INFO_COLLECTED_REVSTATUS_COLLECTED = Integer.valueOf(NumberConstants.NUM31);
+	int STATUS_SERVICE_DETECTCERTINTSLINFOVALIDATION_TSL_FINDED_CERT_DETECTED_INFO_COLLECTED_REVSTATUS_COLLECTED = NumberConstants.NUM31;
 
 }
