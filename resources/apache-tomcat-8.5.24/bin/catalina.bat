d653914ccd94ca0157d73e82f02dc6eb223876d4
JairoFJ <jairo.figueroa@ricoh.es>
2025-06-10 12:01:29 +0200

    BB26983-867: adaptación a log4j2

--- resources/apache-tomcat-8.5.24/bin/catalina.bat
+++ resources/apache-tomcat-8.5.24/bin/catalina.bat
@@ -110,8 +110,8 @@ setlocal
 
 set "JAVA_HOME=C:\Program Files\Java\jdk1.8.0_131"
 set "JAVA_OPTS=%JAVA_OPTS% -Dvalet.config.path=[PROPERTIES SETTING FOLDER PATH]"
-set "JAVA_OPTS=%JAVA_OPTS% -Dlog4j.configuration.valet.rest=[PROPERTIES SETTING FOLDER PATH]/valet-rest-log4j2.xml 
-set "JAVA_OPTS=%JAVA_OPTS% -Dlog4j.configuration.valet.web=[PROPERTIES SETTING FOLDER PATH]/valet-web-log4j2.xml 
+#set "JAVA_OPTS=%JAVA_OPTS% -Dlogging.config=file:[PROPERTIES SETTING FOLDER PATH]/valet-rest-log4j2.xml 
+set "JAVA_OPTS=%JAVA_OPTS% -Dlogging.config=file:[PROPERTIES SETTING FOLDER PATH]/valet-web-log4j2.xml 
 set "JAVA_OPTS=%JAVA_OPTS% -Dspring.config.location=[PROPERTIES SETTING FOLDER PATH]/application.properties"
 set "JAVA_OPTS=%JAVA_OPTS% -Dfile.encoding=UTF-8"
 set "JAVA_OPTS=%JAVA_OPTS% -Dtomcat.server.temp.dir=[PROPERTIES SETTING FOLDER PATH]/tmp/"
