69e9b9ce6e29f3dae01915f9325bba05fcbd7fc6
JairoFJ <jairo.figueroa@ricoh.es>
2025-11-04 12:19:36 +0100

    SGAD1-121: adapatacion de los formularios de usuario a validaciones en backend

--- valet-web/src/main/java/es/gob/valet/controller/UserController.java
+++ valet-web/src/main/java/es/gob/valet/controller/UserController.java
@@ -20,7 +20,7 @@
  * <b>Project:</b><p>Platform for detection and validation of certificates recognized in European TSL.</p>
  * <b>Date:</b><p>15/06/2018.</p>
  * @author Gobierno de España.
- * @version 1.2, 26/12/2018.
+ * @version 1.3, 04/11/2025.
  */
 package es.gob.valet.controller;
 
@@ -31,7 +31,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import es.gob.valet.form.UserForm;
-import es.gob.valet.form.UserFormEdit;
 import es.gob.valet.form.UserFormPassword;
 import es.gob.valet.persistence.ManagerPersistenceServices;
 import es.gob.valet.persistence.configuration.model.entity.UserValet;
@@ -40,7 +39,7 @@ import es.gob.valet.persistence.configuration.services.ifaces.IUserValetService;
 /**
  * <p>Class that manages the requests related to the Users administration.</p>
  * <b>Project:</b><p>Platform for detection and validation of certificates recognized in European TSL.</p>
- * @version 1.2, 26/12/2018.
+ * @version 1.3, 04/11/2025.
  */
 @Controller
 public class UserController {
@@ -56,8 +55,6 @@ public class UserController {
 	 */
 	@RequestMapping(value = "useradmin")
 	public String index(Model model) {
-		model.addAttribute("userFormPassword", new UserFormPassword());
-		model.addAttribute("userformEdit", new UserFormEdit());
 		return "fragments/useradmin.html";
 	}
 
@@ -72,7 +69,6 @@ public class UserController {
 	@RequestMapping(value = "adduser", method = RequestMethod.POST)
 	public String addUser(Model model) {
 		model.addAttribute("userform", new UserForm());
-		model.addAttribute("accion", "add");
 		return "modal/user/userForm";
 	}
 
@@ -99,24 +95,35 @@ public class UserController {
 	/**
 	 * Method that maps the editing of user data to the controller and sets the backing form.
 	 *
-	 * @param username User whose information will be modified.
+	 * @param nif User whose information will be modified.
 	 * @param model Holder object for model attributes.
 	 * @return String that represents the name of the view to forward.
 	 */
-	@RequestMapping(value = "menuedit")
-	public String menuEdit(@RequestParam("username") String username, Model model) {
+	@RequestMapping(value = "editUser", method = RequestMethod.POST)
+	public String menuEdit(@RequestParam("nif") String nif, Model model) {
 		IUserValetService userService = ManagerPersistenceServices.getInstance().getManagerPersistenceConfigurationServices().getUserValetService();
-		UserValet userValet = userService.getUserValetByNif(username);
-		UserFormEdit userFormEdit = new UserFormEdit();
+		UserValet userValet = userService.getUserValetByNif(nif);
+		UserForm userForm = new UserForm();
 
-		userFormEdit.setIdUserValetEdit(userValet.getIdUserValet());
-		userFormEdit.setNameEdit(userValet.getName());
-		userFormEdit.setSurnamesEdit(userValet.getSurnames());
-		userFormEdit.setEmailEdit(userValet.getEmail());
-		userFormEdit.setNifEdit(userValet.getNif());
+		userForm.setIdUserValet(userValet.getIdUserValet());
+		userForm.setName(userValet.getName());
+		userForm.setSurnames(userValet.getSurnames());
+		userForm.setEmail(userValet.getEmail());
+		userForm.setNif(userValet.getNif());
 
-		model.addAttribute("userformEdit", userFormEdit);
-		return "modal/user/userFormEdit.html";
+		model.addAttribute("userform", userForm);
+		return "modal/user/userForm";
 	}
 
+	/**
+	 * Handles POST requests to load the user deletion view.
+	 * 
+	 * @param model the model used to pass attributes to the view
+	 * @return the name of the user deletion modal view
+	 */
+	@RequestMapping(value = "deleteuserview", method = RequestMethod.POST)
+	public String deleteuserview(Model model) {
+		return "modal/user/deleteUser";
+	}
+	
 }
--- valet-web/src/main/java/es/gob/valet/rest/controller/UserRestController.java
+++ valet-web/src/main/java/es/gob/valet/rest/controller/UserRestController.java
@@ -21,17 +21,18 @@
  * <b>Project:</b><p>Platform for detection and validation of certificates recognized in European TSL.</p>
  * <b>Date:</b><p>19/06/2018.</p>
  * @author Gobierno de España.
- * @version 1.4, 19/09/2023.
+ * @version 1.5, 04/11/2025.
  */
 package es.gob.valet.rest.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.validation.constraints.NotEmpty;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
 import org.springframework.data.jpa.datatables.mapping.DataTablesInput;
 import org.springframework.data.jpa.datatables.mapping.DataTablesOutput;
@@ -48,12 +49,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import es.gob.valet.commons.utils.NumberConstants;
 import es.gob.valet.commons.utils.UtilsStringChar;
 import es.gob.valet.form.UserForm;
-import es.gob.valet.form.UserFormEdit;
 import es.gob.valet.form.UserFormPassword;
+import es.gob.valet.i18n.Language;
+import es.gob.valet.i18n.messages.WebGeneralMessages;
 import es.gob.valet.persistence.ManagerPersistenceServices;
 import es.gob.valet.persistence.configuration.model.entity.UserValet;
 import es.gob.valet.persistence.configuration.services.ifaces.IUserValetService;
@@ -64,11 +68,41 @@ import es.gob.valet.utils.GeneralConstantsValetWeb;
  * <p>Class that manages the REST requests related to the Users administration and
  * JSON communication.</p>
  * <b>Project:</b><p>Platform for detection and validation of certificates recognized in European TSL.</p>
- * @version 1.4, 19/09/2023.
+ * @version 1.5, 04/11/2025.
  */
 @RestController
 public class UserRestController {
 
+	/**
+	 * Attribute that represents the object that manages the log of the class.
+	 */
+	private static final Logger LOGGER = LogManager.getLogger(UserRestController.class);
+	
+	/**
+	 * Constant that represents the parameter 'nif'.
+	 */
+	private static final String FIELD_NIF = "nif";
+
+	/**
+	 * Constant that represents the parameter 'name'.
+	 */
+	private static final String FIELD_NAME = "name";
+
+	/**
+	 * Constant that represents the parameter 'surnames'.
+	 */
+	private static final String FIELD_SURNAMES = "surnames";
+
+	/**
+	 * Constant that represents the parameter 'email'.
+	 */
+	private static final String FIELD_EMAIL = "email";
+	
+	/** 
+	 * Regular expression pattern used to validate email addresses. 
+	 */
+	private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
+	
 	/**
 	 * Method that maps the list users web requests to the controller and
 	 * forwards the list of users to the view.
@@ -109,115 +143,99 @@ public class UserRestController {
 
 		return index;
 	}
-
+	
 	/**
-	 * Method that maps the save user web request to the controller and saves it
-	 * in the persistence.
-	 *
-	 * @param userForm
-	 *            Object that represents the backing user form.
-	 * @param bindingResult
-	 *            Object that represents the form validation result.
-	 * @return {@link DataTablesOutput<UserValet>}
+	 * Handles POST requests to save or update a user record.
+	 * Validates the provided user data and persists it if no errors are found.
+	 * 
+	 * @param userForm the form data containing user information
+	 * @return a JSON string containing either validation errors or the saved user data
+	 * @throws IOException if an error occurs while generating the JSON response
 	 */
-	@RequestMapping(value = "/saveuser", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	@JsonView(DataTablesOutput.View.class)
-	public @ResponseBody DataTablesOutput<UserValet> save(@Validated(OrderedValidation.class) @RequestBody UserForm userForm, BindingResult bindingResult) {
-		DataTablesOutput<UserValet> dtOutput = new DataTablesOutput<>();
+	@RequestMapping(value = "/saveuseredit", method = RequestMethod.POST)
+	public @ResponseBody String saveEdit(@RequestBody UserForm userForm) throws IOException {
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		ObjectNode responseNode = objectMapper.createObjectNode();
+		JSONObject json = new JSONObject();
 		UserValet userValet = null;
-		List<UserValet> listNewUser = new ArrayList<UserValet>();
+		boolean error = Boolean.FALSE;
 		IUserValetService userService = ManagerPersistenceServices.getInstance().getManagerPersistenceConfigurationServices().getUserValetService();
-		if (bindingResult.hasErrors()) {
-			listNewUser = StreamSupport.stream(userService.getAllUserValet().spliterator(), false).collect(Collectors.toList());
-			JSONObject json = new JSONObject();
-			for (FieldError o: bindingResult.getFieldErrors()) {
-				json.put(o.getField() + GeneralConstantsValetWeb.SPAN_ELEMENT, o.getDefaultMessage());
+		boolean searchNif = Boolean.FALSE;
+		
+		if (userForm.getIdUserValet() != null) {
+			userValet = userService.getUserValetById(userForm.getIdUserValet());
+			
+			if(!userValet.getNif().equals(userForm.getNif())) {
+				searchNif = Boolean.TRUE;
 			}
-			dtOutput.setError(json.toString());
 		} else {
-			try {
-				if (userForm.getIdUserValet() != null) {
-					userValet = userService.getUserValetById(userForm.getIdUserValet());
-				} else {
-					userValet = new UserValet();
-				}
-
-				userValet.setAttemptsNumber(NumberConstants.NUM0);
-				userValet.setEmail(userForm.getEmail());
-				userValet.setIsBlocked(Boolean.FALSE);
-				userValet.setLastAccess(null);
-				userValet.setLastIpAccess(null);
-				userValet.setName(userForm.getName());
-				userValet.setSurnames(userForm.getSurnames());
-				userValet.setNif(userForm.getNif());
-
-				UserValet user = userService.saveUserValet(userValet);
-
-				listNewUser.add(user);
-			} catch (Exception e) {
-				listNewUser = StreamSupport.stream(userService.getAllUserValet().spliterator(), false).collect(Collectors.toList());
-				throw e;
-			}
+			userValet = new UserValet();
+			searchNif = Boolean.TRUE;
 		}
+		
+		validateUser(userForm, json, userService, searchNif);
+		
+		if (json.length() > 0) {
+			error = Boolean.TRUE;
+			responseNode.set("error", objectMapper.readTree(json.toString()));
+		}
+		
+		if (!error) {
+			
+			userValet.setAttemptsNumber(NumberConstants.NUM0);
+			userValet.setEmail(userForm.getEmail());
+			userValet.setIsBlocked(Boolean.FALSE);
+			userValet.setLastAccess(null);
+			userValet.setLastIpAccess(null);
+			userValet.setName(userForm.getName());
+			userValet.setSurnames(userForm.getSurnames());
+			userValet.setNif(userForm.getNif());
 
-		dtOutput.setData(listNewUser);
-
-		return dtOutput;
-
+			UserValet user = userService.saveUserValet(userValet);
+			
+			responseNode.set("data", objectMapper.valueToTree(user));
+		}
+		
+		return objectMapper.writeValueAsString(responseNode);
+		
 	}
 
 	/**
-	 * Method that maps the save user web request to the controller and saves it
-	 * in the persistence.
+	 * Validates the user form data and populates the provided JSON object with error messages if any fields are invalid.
+	 * Checks NIF format and uniqueness, as well as length and format constraints for name, surnames, and email.
 	 *
-	 * @param userForm Object that represents the backing user form.
-	 * @param bindingResult  Object that represents the form validation result.
-	 * @return  {@link DataTablesOutput<UserValet>}
+	 * @param userForm the form containing user input data
+	 * @param json the JSON object to store validation error messages
+	 * @param userService the service used to query existing users
+	 * @param searchNif flag indicating whether to check for duplicate NIF values
 	 */
-	@RequestMapping(value = "/saveuseredit", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	@JsonView(DataTablesOutput.View.class)
-	public @ResponseBody DataTablesOutput<UserValet> saveEdit(@Validated(OrderedValidation.class) @RequestBody UserFormEdit userForm, BindingResult bindingResult) {
-		DataTablesOutput<UserValet> dtOutput = new DataTablesOutput<>();
-		UserValet userValet = null;
-		List<UserValet> listNewUser = new ArrayList<UserValet>();
-		IUserValetService userService = ManagerPersistenceServices.getInstance().getManagerPersistenceConfigurationServices().getUserValetService();
-		if (bindingResult.hasErrors()) {
-			listNewUser = StreamSupport.stream(userService.getAllUserValet().spliterator(), false).collect(Collectors.toList());
-			JSONObject json = new JSONObject();
-			for (FieldError o: bindingResult.getFieldErrors()) {
-				json.put(o.getField() + GeneralConstantsValetWeb.SPAN_ELEMENT, o.getDefaultMessage());
-			}
-			dtOutput.setError(json.toString());
-		} else {
-			try {
-				if (userForm.getIdUserValetEdit() != null) {
-					userValet = userService.getUserValetById(userForm.getIdUserValetEdit());
-				} else {
-					userValet = new UserValet();
-				}
-				
-				userValet.setAttemptsNumber(NumberConstants.NUM0);
-				userValet.setEmail(userForm.getEmailEdit());
-				userValet.setIsBlocked(Boolean.FALSE);
-				userValet.setLastAccess(null);
-				userValet.setLastIpAccess(null);
-				userValet.setName(userForm.getNameEdit());
-				userValet.setSurnames(userForm.getSurnamesEdit());
-				userValet.setNif(userForm.getNifEdit());
-
-				UserValet user = userService.saveUserValet(userValet);
-
-				listNewUser.add(user);
-			} catch (Exception e) {
-				listNewUser = StreamSupport.stream(userService.getAllUserValet().spliterator(), false).collect(Collectors.toList());
-				throw e;
-			}
+	private void validateUser(UserForm userForm, JSONObject json, IUserValetService userService, boolean searchNif) {
+		if (UtilsStringChar.isNullOrEmpty(userForm.getNif()) || !validateNif(userForm.getNif())) {
+			String msgError = Language.getResWebGeneral(WebGeneralMessages.URC_001);
+			LOGGER.error(msgError);
+			json.put(FIELD_NIF + GeneralConstantsValetWeb.SPAN_ELEMENT, msgError);
+		} else if(searchNif && null != userService.getUserValetByNif(userForm.getNif())) { 
+			String msgError = Language.getResWebGeneral(WebGeneralMessages.URC_005);
+			LOGGER.error(msgError);
+			json.put(FIELD_NIF + GeneralConstantsValetWeb.SPAN_ELEMENT, msgError);
 		}
-
-		dtOutput.setData(listNewUser);
-
-		return dtOutput;
-
+		if (UtilsStringChar.isNullOrEmpty(userForm.getName()) || userForm.getName().length() < NumberConstants.NUM3 &&  userForm.getName().length() > NumberConstants.NUM15) {
+			String msgError = Language.getResWebGeneral(WebGeneralMessages.URC_002);
+			LOGGER.error(msgError);
+			json.put(FIELD_NAME + GeneralConstantsValetWeb.SPAN_ELEMENT, msgError);
+		} 
+		if (UtilsStringChar.isNullOrEmpty(userForm.getSurnames()) || userForm.getSurnames().length() < NumberConstants.NUM3 &&  userForm.getSurnames().length() > NumberConstants.NUM30) {
+			String msgError = Language.getResWebGeneral(WebGeneralMessages.URC_003);
+			LOGGER.error(msgError);
+			json.put(FIELD_SURNAMES + GeneralConstantsValetWeb.SPAN_ELEMENT, msgError);
+		} 
+		if (UtilsStringChar.isNullOrEmpty(userForm.getEmail()) || !isValidEmail(userForm.getEmail())) {
+			String msgError = Language.getResWebGeneral(WebGeneralMessages.URC_004);
+			LOGGER.error(msgError);
+			json.put(FIELD_EMAIL + GeneralConstantsValetWeb.SPAN_ELEMENT, msgError);
+		}
+		
 	}
 
 	/**
@@ -250,4 +268,44 @@ public class UserRestController {
 
 		return result;
 	}
+	
+	/**
+	 * Validates a Spanish NIF (Número de Identificación Fiscal).
+	 * Ensures the format consists of 8 digits followed by an uppercase letter
+	 * and verifies that the letter matches the numeric part according to NIF rules.
+	 *
+	 * @param nif the NIF string to validate
+	 * @return {@code true} if the NIF is valid; {@code false} otherwise
+	 */
+	public boolean validateNif(String nif) {
+	    if (nif == null) return false;
+
+	    // Expresión regular para 8 dígitos seguidos de una letra
+	    Pattern pattern = Pattern.compile("^[0-9]{8}[A-Z]$");
+	    Matcher matcher = pattern.matcher(nif);
+	    if (!matcher.matches()) {
+	        return false;
+	    }
+
+	    String letras = "TRWAGMYFPDXBNJZSQVHLCKET";
+	    String numeroStr = nif.substring(0, 8);
+	    char letra = nif.charAt(8);
+	    int numero = Integer.parseInt(numeroStr);
+	    char letraCorrecta = letras.charAt(numero % 23);
+
+	    return letra == letraCorrecta;
+	}
+	
+	/**
+	 * Validates the format of an email address using a predefined regular expression pattern.
+	 *
+	 * @param email the email address to validate
+	 * @return {@code true} if the email format is valid; {@code false} otherwise
+	 */
+	public static boolean isValidEmail(String email) {
+		if (email == null)
+			return false;
+		return EMAIL_PATTERN.matcher(email).matches();
+	}
+
 }
